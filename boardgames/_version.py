0e8d8224af3b589466f4d069cb88a5cc8793aff3
David Kroon <35101727+dp90@users.noreply.github.com>
2022-09-07 18:42:20 +0200

    update versioneer

--- boardgames/_version.py
+++ boardgames/_version.py
@@ -43,9 +43,9 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = ""
-    cfg.parentdir_prefix = "None"
+    cfg.parentdir_prefix = "board_games-"
     cfg.versionfile_source = "boardgames/_version.py"
     cfg.verbose = False
     return cfg
